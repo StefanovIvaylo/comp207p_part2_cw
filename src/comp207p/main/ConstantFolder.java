032d66d5ac91a51cf84a0fcdda6c22724e48f4a9
Dilan Patel <dilanp@dilans-macbook.home>
2018-04-15 15:06:27 +0100

    started propogation

--- src/comp207p/main/ConstantFolder.java
+++ src/comp207p/main/ConstantFolder.java
@@ -33,56 +33,61 @@ public class ConstantFolder
 		}
 	}
 
-	
+
 	public void optimize()
 	{
 		ClassGen cgen = new ClassGen(original);
 		ConstantPoolGen cpgen = cgen.getConstantPool();
 
-		
+
 		Method[] methods = cgen.getMethods();
-		
+
 		for(Method method : methods) {
 			MethodGen mg = new MethodGen(method, cgen.getClassName(), cpgen);
-			
 
-	        System.out.println(cgen.getClassName() + " > " + method.getName());
-	        System.out.println(mg.getInstructionList());
-	        
-	        
-	        Method improved = improveMethod(mg);
-			
+
+			System.out.println(cgen.getClassName() + " > " + method.getName());
+			System.out.println(mg.getInstructionList());
+
+
+			Method improved = improveMethod(mg);
+
 			cgen.replaceMethod(method, improved);
-	        method = improved;
-	        
-	        mg = new MethodGen(method, cgen.getClassName(), cpgen);
-	        
-	        System.out.println(cgen.getClassName() + " > " + method.getName() + " OPTIMISED!!!!");
-	        System.out.println("Instruction list : " + mg.getInstructionList());
-		}
-		
-        
-		this.optimized = gen.getJavaClass();
-	}
-	
-	private Method improveMethod(MethodGen mg) {
-		
-		InstructionList il = mg.getInstructionList();
-		
-		//We need too loop the following 3 (we might possibly need more) methods
-		//until there are no more changes to be done 
-		
+			method = improved;
 
-			
-			for(int i = 0; i < 15; i++) {
-				simpleFolding(mg, il);
-			}
-			
-			
+			mg = new MethodGen(method, cgen.getClassName(), cpgen);
+
+			System.out.println(cgen.getClassName() + " > " + method.getName() + " OPTIMISED!!!!");
+			System.out.println("Instruction list : " + mg.getInstructionList());
+		}
+
+
+		this.optimized = cgen.getJavaClass();
+	}
+
+	private Method improveMethod(MethodGen mg) {
+
+		InstructionList il = mg.getInstructionList();
+
+		//We need too loop the following 3 (we might possibly need more) methods
+		//until there are no more changes to be done
+
+
+
+		for(int i = 0; i < 30; i++) {
+			simpleFolding(mg, il);
+			propogation(mg, il);
+		}
+
+//			for(int i = 0; i < 1; i++) {
+//			propogation(mg, il);
+//			}
+
+
 //			The following while loop is appropriate for when we have all the functions
-			
+
 //			int count = 1;
-//			
+//
 //			while(count > 0) {
 //				count = 0;
 //				count += simpleFolding(mg, il);
@@ -92,43 +97,42 @@ public class ConstantFolder
 //				//possibly more
 //			}
 
-		
-		
-		
+
+
+
 		mg.setMaxStack();
-	    mg.setMaxLocals();
-	    Method m = mg.getMethod();
-	    il.dispose();
-	    
-	    return m;
+		mg.setMaxLocals();
+		Method m = mg.getMethod();
+		il.dispose();
+
+		return m;
 	}
-	
-	
+
+
 	private int simpleFolding(MethodGen mg, InstructionList il) {
 		ConstantPoolGen cpgen = mg.getConstantPool();
 		InstructionFinder f = new InstructionFinder(il);
-		
+
 		int counter = 0;
-		
+
 		String regexp = "(PushInstruction ConversionInstruction) | (PushInstruction PushInstruction ArithmeticInstruction)";
-		
+
 		for (Iterator i = f.search(regexp); i.hasNext();) {
-			
+
 			InstructionHandle[] match = (InstructionHandle[]) i.next();
-		
-			
+
+
 			if(match.length == 3) {
-				
 				PushInstruction l = (PushInstruction)match[0].getInstruction();
-			    PushInstruction r = (PushInstruction)match[1].getInstruction();
-			    Instruction op = match[2].getInstruction();
-			  
-				
+				PushInstruction r = (PushInstruction)match[1].getInstruction();
+				Instruction op = match[2].getInstruction();
+
+
 				Number ln = getVal(l, cpgen, match);
 				Number rn = getVal(r, cpgen, match);
-				
+
 				if (ln == null || rn == null) {
-		            continue;
+					continue;
 				}
 
 				Instruction fold = arithmeticFold(mg, ln, rn, op);
@@ -136,49 +140,92 @@ public class ConstantFolder
 				match[0].setInstruction(fold);
 
 				try {
-		            il.delete(match[1], match[2]);
-		        }
-		        catch (TargetLostException e) {
-		        	e.printStackTrace();
-		        }
+					il.delete(match[1], match[2]);
+				}
+				catch (TargetLostException e) {
+					e.printStackTrace();
+				}
 
 				counter+=1;
-				
+
 			}
 			else if(match.length == 2) {
-				
+
 				PushInstruction l = (PushInstruction)match[0].getInstruction();
 				ConversionInstruction op = (ConversionInstruction)match[1].getInstruction();
-				
-				
+
+
 				Number ln = getVal(l, cpgen, match);
-				
+
 				if(ln == null) {
 					continue;
 				}
-				
+
 				Instruction fold = conversionFold(mg, ln, op);
-				
+
 				match[0].setInstruction(fold);
-		        try {
-		            il.delete(match[1]);
-		        }
-		        catch (TargetLostException e) {
-		        	e.printStackTrace();
-		        }
-		        counter+=1;
+				try {
+					il.delete(match[1]);
+				}
+				catch (TargetLostException e) {
+					e.printStackTrace();
+				}
+				counter+=1;
 			}
-			
-			
+
+
 		}
-		
+
 		return counter;
 	}
-	
-	
+
+	private int propogation(MethodGen mg, InstructionList il){
+		ConstantPoolGen cpgen = mg.getConstantPool();
+		InstructionFinder f = new InstructionFinder(il);
+
+		int counter = 0;
+
+		String regexp = "ILOAD | DLOAD | FLOAD | LLOAD";
+		InstructionHandle next = null;
+
+		for (Iterator i = f.search(regexp); i.hasNext();){
+			InstructionHandle[] match = (InstructionHandle[]) i.next();
+
+			System.out.println("we have a load instruction");
+			LoadInstruction load = (LoadInstruction)match[0].getInstruction();
+			Number loadedNumber = null;
+			System.out.println(load.getIndex());
+
+			InstructionHandle l = match[0].getPrev();
+
+			while (l != null){
+				Instruction instruction = l.getInstruction();
+
+				if(instruction instanceof StoreInstruction){
+					int matchingIndex = ((StoreInstruction) instruction).getIndex();
+					System.out.println("we got here");
+					System.out.println(l.getPrev());
+
+					if (load.getIndex() == matchingIndex && l.getPrev().getInstruction() instanceof PushInstruction){
+						loadedNumber = getVal((PushInstruction)l.getPrev().getInstruction(), cpgen, match);
+						System.out.println("loadedNumber");
+					}
+				}
+				l = l.getPrev();
+			}
+
+
+			Instruction fold = new LDC(cpgen.addInteger(loadedNumber.intValue()));
+			match[0].setInstruction(fold);
+		}
+
+		return counter;
+	}
+
+
 	private Number getVal(PushInstruction inst, ConstantPoolGen cpgen, InstructionHandle[] match) {
 		Number n = null;
-		
+
 		if (inst instanceof LDC) {
 			n = (Number) ((LDC) inst).getValue(cpgen);
 		}
@@ -192,149 +239,149 @@ public class ConstantFolder
 			n = (Number) ((ConstantPushInstruction) inst).getValue();
 		}
 		else if(inst instanceof LoadInstruction) {
-			
+
 			InstructionHandle ih = match[0];
 			int index = ((LoadInstruction) inst).getIndex();
 			InstructionHandle i = match[0].getPrev();
-			
+
 			while (i != null ) {
 				Instruction instruction = i.getInstruction();
-				
+
 				if(instruction instanceof StoreInstruction) {
 					int currIndex = ((StoreInstruction) instruction).getIndex();
-					
-					
+
+
 					if(index == currIndex && i.getPrev().getInstruction() instanceof PushInstruction) {
 
 						match[0].setInstruction(i.getPrev().getInstruction());
-						
+
 						getVal((PushInstruction)match[0].getInstruction(), cpgen, match);
-						
+
 						break;
 					}
 				}
-				
+
 				i = i.getPrev();
 			}
 		}
-		
+
 		return n;
 	}
-	
+
 	private Instruction conversionFold(MethodGen mg, Number b,  Instruction op) {
 		ConstantPoolGen cpgen = mg.getConstantPool();
-	    Instruction inst = null;
-	    
-	    if(op instanceof I2D) {
-	    	inst = new LDC2_W(cpgen.addDouble((double) b.doubleValue()));
-	    }
-	    else if(op instanceof I2F) {
-	    	inst = new LDC(cpgen.addFloat((float) b.floatValue()));
-	    }
-	    else if(op instanceof I2L) {
-	    	inst = new LDC2_W(cpgen.addLong((long) b.longValue()));
-	    }
-	    else if(op instanceof D2F) {
-	    	inst = new LDC(cpgen.addFloat((float) b.floatValue()));
-	    }
-	    else if(op instanceof D2I) {
-	    	inst = new LDC(cpgen.addInteger((int) b.intValue()));
-	    }
-	    else if(op instanceof D2L) {
-	    	inst = new LDC2_W(cpgen.addLong((long) b.longValue()));
-	    }
-	    else if(op instanceof F2D) {
-	    	inst = new LDC2_W(cpgen.addDouble((double) b.doubleValue()));
-	    }
-	    else if(op instanceof F2I) {
-	    	inst = new LDC(cpgen.addInteger((int) b.intValue()));
-	    }
-	    else if(op instanceof F2L) {
-	    	inst = new LDC2_W(cpgen.addLong((long) b.longValue()));
-	    }
-	    else if(op instanceof L2I) {
-	    	inst = new LDC(cpgen.addInteger((int) b.intValue()));
-	    }
-	    else if(op instanceof L2D) {
-	    	inst = new LDC2_W(cpgen.addDouble((double) b.doubleValue()));
-	    }
-	    else if(op instanceof L2F) {
-	    	inst = new LDC(cpgen.addFloat((float) b.floatValue()));
-	    }
-	    
-	    
-	    return inst;
-	}
-	
-	private Instruction arithmeticFold(MethodGen mg, Number a, Number b,  Instruction op) {
-	    ConstantPoolGen cpgen = mg.getConstantPool();
-	    Instruction inst = null;
-	    
-	    if (op instanceof IADD) {
-	    	inst = new LDC(cpgen.addInteger(a.intValue() + b.intValue()));
-	    }
-	    else if (op instanceof ISUB) {
-	    	inst = new LDC(cpgen.addInteger(a.intValue() - b.intValue()));
-	    }
-	    else if (op instanceof IMUL) {
-	    	inst = new LDC(cpgen.addInteger(a.intValue() * b.intValue()));
-	    }
-	    else if (op instanceof IDIV) {
-	    	inst = new LDC(cpgen.addInteger(a.intValue() / b.intValue()));
-	    }
-	    else if (op instanceof IREM) {
-	    	inst = new LDC(cpgen.addInteger(a.intValue() % b.intValue()));
-	    }
-	    else if (op instanceof DADD) {
-	    	inst = new LDC2_W(cpgen.addDouble(a.doubleValue() + b.doubleValue()));
-	    }
-	    else if (op instanceof DSUB) {
-	    	inst = new LDC2_W(cpgen.addDouble(a.doubleValue() - b.doubleValue()));
-	    }
-	    else if (op instanceof DMUL) {
-	    	inst = new LDC2_W(cpgen.addDouble(a.doubleValue() * b.doubleValue()));
-	    }
-	    else if (op instanceof DDIV) {
-	    	inst = new LDC2_W(cpgen.addDouble(a.doubleValue() / b.doubleValue()));
-	    }
-	    else if (op instanceof DREM) {
-	    	inst = new LDC2_W(cpgen.addDouble(a.doubleValue() % b.doubleValue()));
-	    }
-	    else if (op instanceof FADD) {
-	    	inst = new LDC(cpgen.addFloat(a.floatValue() + b.floatValue()));
-	    }
-	    else if (op instanceof FSUB) {
-	    	inst = new LDC(cpgen.addFloat(a.floatValue() - b.floatValue()));
-	    }
-	    else if (op instanceof FMUL) {
-	    	inst = new LDC(cpgen.addFloat(a.floatValue() * b.floatValue()));
-	    }
-	    else if (op instanceof FDIV) {
-	    	inst = new LDC(cpgen.addFloat(a.floatValue() / b.floatValue()));
-	    }
-	    else if (op instanceof FREM) {
-	    	inst = new LDC(cpgen.addFloat(a.floatValue() % b.floatValue()));
-	    }
-	    else if (op instanceof LADD) {
-	    	inst = new LDC2_W(cpgen.addLong(a.longValue() + b.longValue()));
-	    }
-	    else if (op instanceof LSUB) {
-	    	inst = new LDC2_W(cpgen.addLong(a.longValue() - b.longValue()));
-	    }
-	    else if (op instanceof LMUL) {
-	    	inst = new LDC2_W(cpgen.addLong(a.longValue() * b.longValue()));
-	    }
-	    else if (op instanceof LDIV) {
-	    	inst = new LDC2_W(cpgen.addLong(a.longValue() / b.longValue()));
-	    }
-	    else if (op instanceof LREM) {
-	    	inst = new LDC2_W(cpgen.addLong(a.longValue() % b.longValue()));
-	    }
-	    
-	    return inst;
-}
+		Instruction inst = null;
 
-	
+		if(op instanceof I2D) {
+			inst = new LDC2_W(cpgen.addDouble((double) b.doubleValue()));
+		}
+		else if(op instanceof I2F) {
+			inst = new LDC(cpgen.addFloat((float) b.floatValue()));
+		}
+		else if(op instanceof I2L) {
+			inst = new LDC2_W(cpgen.addLong((long) b.longValue()));
+		}
+		else if(op instanceof D2F) {
+			inst = new LDC(cpgen.addFloat((float) b.floatValue()));
+		}
+		else if(op instanceof D2I) {
+			inst = new LDC(cpgen.addInteger((int) b.intValue()));
+		}
+		else if(op instanceof D2L) {
+			inst = new LDC2_W(cpgen.addLong((long) b.longValue()));
+		}
+		else if(op instanceof F2D) {
+			inst = new LDC2_W(cpgen.addDouble((double) b.doubleValue()));
+		}
+		else if(op instanceof F2I) {
+			inst = new LDC(cpgen.addInteger((int) b.intValue()));
+		}
+		else if(op instanceof F2L) {
+			inst = new LDC2_W(cpgen.addLong((long) b.longValue()));
+		}
+		else if(op instanceof L2I) {
+			inst = new LDC(cpgen.addInteger((int) b.intValue()));
+		}
+		else if(op instanceof L2D) {
+			inst = new LDC2_W(cpgen.addDouble((double) b.doubleValue()));
+		}
+		else if(op instanceof L2F) {
+			inst = new LDC(cpgen.addFloat((float) b.floatValue()));
+		}
+
+
+		return inst;
+	}
+
+	private Instruction arithmeticFold(MethodGen mg, Number a, Number b,  Instruction op) {
+		ConstantPoolGen cpgen = mg.getConstantPool();
+		Instruction inst = null;
+
+		if (op instanceof IADD) {
+			inst = new LDC(cpgen.addInteger(a.intValue() + b.intValue()));
+		}
+		else if (op instanceof ISUB) {
+			inst = new LDC(cpgen.addInteger(a.intValue() - b.intValue()));
+		}
+		else if (op instanceof IMUL) {
+			inst = new LDC(cpgen.addInteger(a.intValue() * b.intValue()));
+		}
+		else if (op instanceof IDIV) {
+			inst = new LDC(cpgen.addInteger(a.intValue() / b.intValue()));
+		}
+		else if (op instanceof IREM) {
+			inst = new LDC(cpgen.addInteger(a.intValue() % b.intValue()));
+		}
+		else if (op instanceof DADD) {
+			inst = new LDC2_W(cpgen.addDouble(a.doubleValue() + b.doubleValue()));
+		}
+		else if (op instanceof DSUB) {
+			inst = new LDC2_W(cpgen.addDouble(a.doubleValue() - b.doubleValue()));
+		}
+		else if (op instanceof DMUL) {
+			inst = new LDC2_W(cpgen.addDouble(a.doubleValue() * b.doubleValue()));
+		}
+		else if (op instanceof DDIV) {
+			inst = new LDC2_W(cpgen.addDouble(a.doubleValue() / b.doubleValue()));
+		}
+		else if (op instanceof DREM) {
+			inst = new LDC2_W(cpgen.addDouble(a.doubleValue() % b.doubleValue()));
+		}
+		else if (op instanceof FADD) {
+			inst = new LDC(cpgen.addFloat(a.floatValue() + b.floatValue()));
+		}
+		else if (op instanceof FSUB) {
+			inst = new LDC(cpgen.addFloat(a.floatValue() - b.floatValue()));
+		}
+		else if (op instanceof FMUL) {
+			inst = new LDC(cpgen.addFloat(a.floatValue() * b.floatValue()));
+		}
+		else if (op instanceof FDIV) {
+			inst = new LDC(cpgen.addFloat(a.floatValue() / b.floatValue()));
+		}
+		else if (op instanceof FREM) {
+			inst = new LDC(cpgen.addFloat(a.floatValue() % b.floatValue()));
+		}
+		else if (op instanceof LADD) {
+			inst = new LDC2_W(cpgen.addLong(a.longValue() + b.longValue()));
+		}
+		else if (op instanceof LSUB) {
+			inst = new LDC2_W(cpgen.addLong(a.longValue() - b.longValue()));
+		}
+		else if (op instanceof LMUL) {
+			inst = new LDC2_W(cpgen.addLong(a.longValue() * b.longValue()));
+		}
+		else if (op instanceof LDIV) {
+			inst = new LDC2_W(cpgen.addLong(a.longValue() / b.longValue()));
+		}
+		else if (op instanceof LREM) {
+			inst = new LDC2_W(cpgen.addLong(a.longValue() % b.longValue()));
+		}
+
+		return inst;
+	}
+
+
 	public void write(String optimisedFilePath)
 	{
 		this.optimize();
